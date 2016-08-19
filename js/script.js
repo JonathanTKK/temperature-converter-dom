0e0a8da60135b5737020d2aaeac04065fe73cfe9
Jonathan Tan <Jonalogy@Jonathans-MacBook-Air.local>
2016-08-19 15:50:46 +0800

    Remove test toggle button.

--- js/script.js
+++ js/script.js
@@ -6,15 +6,16 @@ document.getElementById('submit').addEventListener('click',getInput);
 
 document.getElementById('clear').addEventListener('click',clear);
 
-//document.getElementById('toggle').addEventListener('click',toggle);
 
+
+//document.getElementById('toggle').addEventListener('click',toggle);
 
 //-----Functions for Buttons-----
   //Collects user input data
   function getInput(){
     //console.log("getInput() initialised"); //For testing purpose
     userInput = document.getElementById("input").value;
-    console.log("User entered : " + userInput);
+    //console.log("User entered : " + userInput);
     if (userInput === ""){
       document.getElementById('result').textContent = "No input detected";
     }
@@ -30,43 +31,43 @@ document.getElementById('clear').addEventListener('click',clear);
     document.getElementById('result').textContent = "Input value to convert";
   }
 
-  // //Toggles color
-  // function toggle(){
-  //   if(toggleCheck === 1){
-  //     document.getElementById('output').style.backgroundColor = "red";
-  //     toggleCheck = 0;
-  //   }
-  //   else{
-  //     document.getElementById('output').style.backgroundColor = "blue";
-  //     toggleCheck = 1
-  //   }}
-
 //-----Functions for conversion-----
 
   function convert(){
     //console.log("degCel() initialised"); //For testing purpose
     //console.log(document.getElementById('fahren').checked===true);//For testing purpose only
     if (document.getElementById("fahren").checked===true){
-      output = (Math.round(((userInput * 1.8)+32)*10))/10;
+      output = ((Math.round(((userInput * 1.8)+32)*10))/10);
       //console.log(output);//For testing
-      if(output>95){
+
+      if(output>50 && output<95){
+        document.getElementById('output').style.backgroundColor = "white";
+      }
+      else if(output>95){
         document.getElementById('output').style.backgroundColor = "red";
       }
       else if (output<50) {
         document.getElementById('output').style.backgroundColor = "blue";
       }
 
-      document.getElementById('result').textContent = output;
+
+      document.getElementById('result').textContent = output + "°C" ;
     }
     if(document.getElementById("celcius").checked===true){
       output = (Math.round((userInput-32)/(5/9))/10);
       //console.log(output);//For testing
-      if(output>35){
-        document.getElementById('output').style.backgroundColor = "red";
+
+      if(output>10 && output<35){
+          document.getElementById('output').style.backgroundColor = "white";
       }
+      else if(output>35){
+        document.getElementById('output').style.backgroundColor = "red";
+        }
       else if (output<10) {
         document.getElementById('output').style.backgroundColor = "blue";
+        }
+      document.getElementById('result').textContent = output + "°F";
       }
-      }
-      document.getElementById('result').textContent = output;
+
+
     }
